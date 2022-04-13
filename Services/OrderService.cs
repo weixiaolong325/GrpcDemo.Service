04ffc49f6a9cc54c79206c9215f815caf61c9f6f
wxl_home <284743503@qq.com>
2022-04-14 01:24:24 +0800

    添加项目文件。

---
+++ Services/OrderService.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using System;
+using static GrpcDemo.Service.QueryResult.Types;
+
+namespace GrpcDemo.Service.Services
+{
+    public class OrderService:Order.OrderBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public OrderService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// 创建订单
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<CreateResult> CreateOrder(CreateRequest request, ServerCallContext context)
+        {
+            //报存数据库 todo
+
+            return Task.FromResult(new CreateResult
+            {
+                Result=true,
+                Message="订单创建成功"
+            });
+        }
+        /// <summary>
+        /// 查询订单
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<QueryResult> QueryOrder(QueryRequest request, ServerCallContext context)
+        {
+            //查询数据库 //todo
+
+            return Task.FromResult(new QueryResult
+            {
+                OrderInfo=new OrderInfo
+                {
+                    Id = request.Id,
+                    OrderNo = DateTime.Now.ToString("yyyyMMddHHmmss"),
+                    OrderName = "冰箱",
+                    Price = 1288
+                }
+            });
+        }
+    }
+}
