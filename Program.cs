f416ca2a43879070cf044ad85a8f88ac3efa0948
wxl_home <284743503@qq.com>
2022-04-16 23:04:51 +0800

    增加gRPC注册到Consul代码

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using GrpcDemo.Service.Services;
+using GrpcDemo.Service.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+//≈‰÷√ªÒ»°
+
+
 var app = builder.Build();
+IConfiguration _configuration = builder.Configuration;
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<OrderService>();
+app.MapGrpcService<HealthCheckService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
+app.UseConsul(_configuration);
 app.Run();
